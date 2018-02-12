5b40d37fdf5fa9aadc8ec18ed8bfc914a274c6c9
JO <jesseoldershaw@yahoo.co.uk>
2018-02-12 20:31:05 +0000

    Simple dummy web interface based on python Flask framework

---
+++ src/ptp.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route('/index')
+def index():  
+    '''Web page accessible via http://localhost/index. Has 'start capture' button.'''
+    return render_template('index.html')
+
+
+@app.route('/capture')
+def capture():
+    '''Web page accessible via http://localhost/capture. Has 'stop capture and see results' button.'''
+    start_capture()
+    return render_template('capturing.html')
+
+
+@app.route('/results')
+def results():
+    '''Web page accessible via http://localhost/capture. Has results of capture, and a button to return to index page.'''
+    stop_capture()
+    results = generate_analysis()
+    return render_template('results.html', results=results)
+
+
+def start_capture():
+    '''Starts capturing traffic involving the target device.'''
+    log("start_capture(): traffic capture started")
+
+
+def stop_capture():
+    '''Stops capturing traffic involving the target device.'''
+    log("stop_capture(): traffic capture stopped")
+
+
+def generate_analysis():
+    '''Analyses captured traffic involving the target device'''
+    log("analyse_captured_traffic(): analysed")
+    return "Dummy results"
+
+
+def log(msg): 
+    '''Writes to a log file, for debugging purposes'''
+    log_file = "ptp.log"
+    msg += "\n"
+    with open(log_file, "a") as f:
+        f.write(msg)
+
+
+if __name__ == "__main__":
+    app.run()
