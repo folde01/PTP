269debee06c0c0975922c9e4946c0296ed5fdcc8
JO <jo6543@yahoo.com>
2018-03-12 17:36:05 +0000

    server class needed?

--- src/ptp_analyser.py
+++ src/ptp_analyser.py
@@ -10,20 +10,15 @@ class Analyser:
     def _total_bytes_from_client(self):
         return sum([ stream.server.count for stream in self._streams]) 
 
-
     def _total_bytes_to_client(self):
         return sum([ stream.client.count for stream in self._streams]) 
-
 
     def results(self):
         self._analyse_pcapfile(self._pcap_filename())
         return self._results
 
-
     def _pcap_filename(self):
         return self._sniffer.pcap_filename()
-
-
 
     def _analyse_pcapfile(self, pcap_filename):
         nids.param("scan_num_hosts", 0) # disable portscan detection
@@ -31,14 +26,26 @@ class Analyser:
         nids.param("filename", pcap_filename)
         nids.init()
         #nids.register_tcp(self._tcpStreamCallback)
-        nids.register_tcp(self._callback_totalBytes)
+        #nids.register_tcp(self._callback_totalBytes)
+        nids.register_tcp(self._callback_gatherStreamObjects)
         nids.run()
-        self._results.append("TOTAL BYTES FROM CLIENT: " + str(self._total_bytes_from_client()))
-        self._results.append("TOTAL BYTES TO CLIENT: " + str(self._total_bytes_to_client()))
+        #self._results.append("TOTAL BYTES FROM CLIENT: " + str(self._total_bytes_from_client()))
+        #self._results.append("TOTAL BYTES TO CLIENT: " + str(self._total_bytes_to_client()))
 
+            
     def _add_stream_if_new(self, stream):
         if stream not in self._streams:
             self._streams.append(stream)
+
+    def _get_stream_objects
+
+    def _callback_gatherStreamObjects(self, tcpStream):
+        self._add_stream_if_new(tcpStream)
+        if tcpStream.nids_state == nids.NIDS_JUST_EST:
+            tcpStream.client.collect = 1
+            tcpStream.server.collect = 1
+        else: 
+            tcpStream.discard(0)
 
     def _callback_totalBytes(self, tcpStream):
         if tcpStream.nids_state == nids.NIDS_JUST_EST:
@@ -79,3 +86,22 @@ class Analyser:
             self._results.append(result)
             result = str(tcpStream.addr) + " -- TOTAL bytes to client: " + str(tcpStream.client.count)
             self._results.append(result)
+
+    def _streams_to_servers(self):
+        servers = []
+        for stream in self._streams:
+            ((src_ip,src_port),(dest_ip,dest_port)) = stream.addr
+            server = Server(dest_ip, dest_port, stream.server.count, stream.client.count)
+            if server not in servers:
+                servers.append(server)
+            else: 
+
+class Server:
+
+    def __init__(self, ip_addr, tcp_port, bytes_to_server, bytes_to_client):
+        self.ip_addr = ip_addr
+        self.tcp_port = tcp_port
+        self.bytes_to_server = bytes_to_server 
+        self.bytes_to_client = bytes_to_client 
+
+
