5d4efdd532d8a0d895d35fcdb461a85e4202498d
JO <jo6543@yahoo.com>
2018-09-17 18:10:32 +0100

    session pair doc

--- src/ptp_session_pair.py
+++ src/ptp_session_pair.py
@@ -1,5 +1,4 @@
-#from scapy.all import rdpcap, PacketList, TCP, Raw
-from scapy.all import *
+from scapy.all import rdpcap, PacketList, TCP, Raw
 from ptp_network import Network
 from ptp_constants import Constants, Is_Encrypted_Enum
 from ptp_connection_status import Stream_Status, TCP_Status, SSL_Status
@@ -9,6 +8,32 @@ import re
 import ptp_ssl_ciphers
 
 class Session_Pair(object):
+    """A session pair is the two-way traffic of a TCP connection, i.e.
+    between a) a unique client IP address and TCP port pair) and 
+    b) a unique server IP address and TCP port pair. The word session 
+    comes from the Scapy sessions() method which separates packets 
+    into Scapy PacketList objects which contain the packets representing 
+    a single direction of this traffic (client-to-server, or 
+    server-to-client). Session_Pair exists in this form so that the 
+    two directions can be looked at together, for the sake of 
+    Application Layer (SSL) protocol analysis.
+
+    Args:
+        cli_to_svr (PacketList): packets sent from client to server 
+        svr_to_cli (PacketList): packets sent from server to client 
+
+    Attributes:
+        _cli_to_svr (PacketList): packets sent from client to server
+        _svr_to_cli (PacketList): packets sent from server to client
+        _stream_status (Stream_Status): PTP Stream_Status object containing overall
+            TCP and SSL protocol analysis of the session pair
+        _tcp_status (TCP_Status): PTP TCP_Status object containing TCP analysis of
+        _ssl_status (SSL_Status): PTP SSL_Status object containing SSL analysis of
+            session pair 
+        _const (Constants): PTP Constants object, for obtaining constant values
+        _enum (Is_Encrypted_Enum): enumeration of YES, NO and UNKNOWN
+
+    """
 
     def __init__(self, cli_to_svr, svr_to_cli):
         self._cli_to_svr = cli_to_svr
@@ -17,13 +42,16 @@ class Session_Pair(object):
         self._tcp_status = None
         self._ssl_status = SSL_Status()
 	self._const = Constants() 
+        self._enum = Is_Encrypted_Enum()
 
     def get_stream_status(self):
-        '''
+        """Calls on other protocol analysis methods of this class, combines
+        their results and returns a single results object.
+
         Returns: 
-            A Stream object containing the full analysis of the session pair.
+            A Stream_Status object containing the full analysis of the session pair.
             This has all the info we need for the stream database.
-        '''
+        """
 
         stream_status = Stream_Status(tcp_status=self._get_tcp_status(),
                                       ssl_status=self._get_ssl_status())
@@ -32,10 +60,29 @@ class Session_Pair(object):
 
 
     def _get_tcp_status(self):
-        '''
+        """Analyses the two-directional traffic of the Session Pair and 
+        gathers TCP status information into a TCP_Status object, e.g.
+        client IP address and TCP port, server IP address and TCP port,
+        total bytes sent to server, total bytes sent to server, start
+        and finish timestamps. Handles session pairs which may contain traffic
+        in only one direction (see note).
+
+        Note:
+        A session in a session pair may not contain any packets. This would
+        be the case if a session pair contained only traffic going in a
+        single direction. In this case, packet 0 of a session doesn't exist,
+        and an exception arises, giving us the chance to correctly assign
+        Scapy src and dest values to cli_ip and and svr_ip.
+
+        Note:
+        We assume that TCP Fast Open (which there are reportedly still
+        a lot of problems with, preventing wide adoption) is not used,
+        so e.g. SSL Client Hello is still sent in client's third packet (as
+        opposed to earlier).
+
         Returns: 
             A TCP_Status object containing the TCP analysis of the session pair.
-        '''
+        """
 
         if self._tcp_status is not None:
             return self._tcp_status
@@ -43,12 +90,6 @@ class Session_Pair(object):
         cli_to_svr_session = self._cli_to_svr 
         svr_to_cli_session = self._svr_to_cli 
 
-        '''
-        A session in a session pair may not contain any packets. This would be the case if a session pair
-        contained only traffic going in a single direction. In this case, packet 0 of a session doesn't exist,
-        and an exception arises, giving us the chance to correctly assign Scapy src and dest values to 
-        cli_ip and and svr_ip.
-        '''
         try:
             cli_ip = cli_to_svr_session[0]['IP'].src
             cli_pt = cli_to_svr_session[0]['TCP'].sport
@@ -67,8 +108,6 @@ class Session_Pair(object):
         bytes_to_cli = self._get_session_payload_size(svr_to_cli_session)
         ts_first_pkt, ts_last_pkt = self._get_start_and_end_ts()
 
-        # Assumes for now that TCP Fast Open is not used, so Client Hello is sent in
-        # client's third packet.
 
 	tcp_status = TCP_Status(cli_ip=cli_ip, cli_pt=int(cli_pt), svr_ip=svr_ip,
                 svr_pt=int(svr_pt), bytes_to_cli=bytes_to_cli,
@@ -80,13 +119,13 @@ class Session_Pair(object):
  
 
     def _get_session_payload_size(self, session):
-        '''
+        """Sums up session payload in bytes 
+
         Args:
-            session (PacketList): Scapy packet list representing one direction of
-            TCP connection.
+            session (PacketList): one direction of TCP connection
         Returns:
             int: size of total TCP payload for all packets in bytes
-        '''
+        """
         if session is None:
             return 0
         size = 0
@@ -96,8 +135,13 @@ class Session_Pair(object):
 
 
     def _get_start_and_end_ts(self):
-        '''returns two-tuple containing lowest and highest timestamps in Epoch seconds, in microseconds.
-        returns None if session is None'''
+        """Finds lowest and highest packet timestamps found in session, thus the 
+        start and finish of observed connection activity. Uses UNIX Epoch time,
+        in microseconds. Returns None if session is None.
+
+        Returns:
+            (int,int): lowest timestamp found (session start), highest (session end)
+        """
 
 	cli_to_svr_session = self._cli_to_svr
 	svr_to_cli_session = self._svr_to_cli
@@ -119,33 +163,38 @@ class Session_Pair(object):
         return (lowest_ts, highest_ts)
 
 
-    def _get_pkt_payload_length(self, pkt):
-        return len(pkt[TCP].payload)
-
 
     def _get_ssl_status(self):
-        '''Examine use (or otherwise) of SSL protocol.
+        """Populates the SSL_Status object by calling all the methods which
+        write to it.
 
         Returns:
             SSL_Status object containing details of SSL use.
-        '''
+        """
         self._ssl_handshake_client_analysis()
         self._ssl_handshake_server_analysis()
         self._is_encrypted()
-        #self._ssl_tunnel_analysis()
-        #self._tcp_close_analysis()
         return self._ssl_status 
 
+
     def _is_encrypted(self):
+        """Sets criteria for decision on encryption use, makes decision based
+        on this and the data gathered by other methods and stored in
+        Status_Status object. Populates that object with its decision. Its
+        decision can be YES, NO or UNKNOWN (which come from a 3-way enumeration).
+
+        Note:
+            The criteria for YES is set out below in ssl_handshake_reqs.
+            The criteria for NO is set out below in unencrypted_reqs.
+            Otherwise, it returns UNKNOWN.
+        """
         ss = self._ssl_status
         ts = self._tcp_status
-
-        ssl_handshake_reqs = [ ss.ssl_cli_hello, ss.ssl_cli_ccs, ss.ssl_svr_hello, ss.ssl_svr_ccs ] 
+        ssl_handshake_reqs = \
+            [ ss.ssl_cli_hello, ss.ssl_cli_ccs, ss.ssl_svr_hello, ss.ssl_svr_ccs ] 
         unencrypted_reqs = [ts.svr_pt == 80, not all(ssl_handshake_reqs) ] 
-        
         result = None
-
-        enum = Is_Encrypted_Enum()
+        enum = self._enum 
 
         if all(ssl_handshake_reqs):
             result = enum.YES 
@@ -159,15 +208,19 @@ class Session_Pair(object):
 
 
     def _get_load(self, pkt):
-        '''
+        """Extracts TCP payload found in Scapy Packet.
+
         Args:
-            pkt (scapy.all.Packet)
+            pkt (Packet)
+
         Returns:
             str: TCP payload of a single packet as a string of hex digits,
-                from Raw layer of scapy packet.
+                converted from Raw layer of Scapy packet. Hex is used as the
+                SSL byte codes are humanly familiar in hex (e.g. version
+                numbers).
         Raises:
             TypeError: If packet has no payload.
-        '''
+        """
         if pkt.haslayer(Raw):
             load = pkt[TCP][Raw].load
             return load.encode('HEX')
@@ -175,24 +228,27 @@ class Session_Pair(object):
             raise TypeError("Packet has no payload.")  
 
 
+
     def _ssl_handshake_client_analysis(self):
+        """Carries out the analysis of the client side of the SSL handshake
+        and populates this class's SSL_Status object.
+        """
+
         status = self._ssl_status
 	const = self._const.ssl
 	pkt_seq = self._cli_to_svr
 
-        if pkt_seq is None:
-            #print "cli_to_svr is None"
-            return
+        if pkt_seq is None: return
 
 	pkt_seq_load = ''
 
-        # First two packets should be enough in most cases but we increase 
-	# it to account for any TCP segmentation or use of client authentication.
-        #first_n_packets = 4 # not enough!  
+        # The number of packets at the beginning of the session that we'll
+        # search. Too high and we might get false positives. Too low and we
+        # miss this side  handshake. Remember, this is just one side of the
+        # handshake.
         first_n_packets = 8     
 
 	num_pkts_with_payload = [p.haslayer(Raw) for p in pkt_seq].count(True)
-        #print 'num_pkts_with_payload:', num_pkts_with_payload
 
         # concatenate payloads of first packets
 	if num_pkts_with_payload < first_n_packets:
@@ -203,7 +259,6 @@ class Session_Pair(object):
                 if p.haslayer(Raw):
                     pkt_seq_load += self._get_load(p) 
             
-        #print "pkt_seq_load:", pkt_seq_load
 
         # The payload matches a regex if it has both Client Hello (CH) and Change Cipher
         # Suite (CCS) messages (in that order, with any bytes in between).
@@ -236,13 +291,9 @@ class Session_Pair(object):
 	    ''', re.VERBOSE | re.IGNORECASE)
 
 	match = regex.match(pkt_seq_load)
-        #print 'match:', match
 
         if match:
             groups = match.groups()
-
-            #print "groups:", groups
-
             client_hello_group_index = 0
             ccs_group_index = 1
             client_hello_seen = bool(groups[client_hello_group_index])
@@ -250,29 +301,20 @@ class Session_Pair(object):
             client_ccs_seen = bool(groups[ccs_group_index])
             self._ssl_status.ssl_cli_ccs = client_ccs_seen
 
-            #print 'client_hello_seen:', client_hello_seen
-            #print 'client_ccs_seen:', client_ccs_seen
-            #print "ssl_status.show():", self._ssl_status.show()
-
 
     def _ssl_handshake_server_analysis(self):
+        """Carries out the analysis of the server side of the SSL handshake
+        and populates this class's SSL_Status object.
+        """
+
 	const = self._const.ssl
 	pkt_seq = self._svr_to_cli
 
         if pkt_seq is None:
-            #print "svr_to_cli is None"
             return
         
 	pkt_seq_load = ''
 
-        '''
-        Note about setting first_n_packets: although it may work most of the time, a 
-        value of 2 (i.e. the first two payloaded packets) is too low as we must account
-        for any TCP segmentation or use of client authentication. However, setting first_n_packets
-        too high means we waste time searching payloads unlikely to contain anything 
-        we need, and we increase the likelihood that the patterns we are looking for 
-        appear elsewhere but in a different context.
-        '''
         first_n_packets = 8
 
         num_pkts_with_payload = [p.haslayer(Raw) for p in pkt_seq].count(True)
@@ -320,11 +362,9 @@ class Session_Pair(object):
 	match = regex.match(pkt_seq_load)
 
         # Don't bother with subsequent search if no match.
-        if not match:
-            return
+        if not match: return
 
         groups = match.groups()
-        #print 'groups:', groups
         
         # Set whether Server Hello seen
         handshake_record_group = 0
@@ -344,14 +384,12 @@ class Session_Pair(object):
         length_session_id_group = 3
         length_session_id = 2 * int(groups[length_session_id_group], 16)
 
-        #print "SERVER HELLO -- ssl_version:", ssl_version, "length_session_id:", length_session_id, \
-        #    "groups:", groups
 
-        '''
+        """
         With session ID length in hand, we now search the next part of the payload,
         starting with where we left off, and looking for a session ID of the
         specified length, followed by the cipher suite.
-        '''
+        """
 
         re_session_id = r'[0-9a-f]{' + re.escape(str(length_session_id)) + r'}'
 
@@ -395,7 +433,6 @@ class Session_Pair(object):
 
         if match:
             groups = match.groups()
-            #print 'groups:', groups
 
             cipher_suite_group = 4
             cipher_code = groups[cipher_suite_group] # TODO: deal with IndexError if not
@@ -406,77 +443,3 @@ class Session_Pair(object):
             start_of_encrypted_tunnel_group = 6
             self._ssl_status.ssl_svr_ccs = bool(groups[change_cipher_spec_group]) \
                     and bool(groups[start_of_encrypted_tunnel_group])
-
-            #print "matched_all:", matched_all, "ssl_version:", ssl_version, \
-            #    "cipher_suite:", cipher_suite, "groups:", groups,
-
-        #print "ssl_status.show():", self._ssl_status.show()
-
-
-
-    def _ssl_handshake_analysis_old(self):
-        '''Updates SSL_Status object for this packet based on first packets exchanged.
-        If TCP handshake is seen then examine the subsequent exchange of messages (sometimes called
-        the 'four flights') required to establish the encrypted tunnel, 
-        namely Client Hello, Server Hello, plus a Change Cipher Suite from client and server.
-        Updates SSL status object with SSL version and cipher
-        used. Assumes TCP Fast Open is not used, so Client Hello must be sent 
-        in client's third packet.
-        '''
-        ssl_status = self._ssl_status
- 
-        if not self._tcp_handshake_is_seen():
-            ssl_status.ssl_handshake_seen = False
-        else:
-            try:
-                flights = (self._cli_to_svr[2],
-                           self._svr_to_cli[2],
-                           self._cli_to_svr[3],
-                           self._svr_to_cli[3])
-            except IndexError:
-                print "Not enough packets for SSL handshake"
-                ssl_status.ssl_handshake_seen = False
-                return
-
-        flight_payloads = [TCP_Payload(flights[i]) for i in range(0,4)]
-
-        # TODO: add try/except block for False case
-        if (flight_payloads[0].is_ssl_client_hello()
-                and flight_payloads[1].is_ssl_server_hello()
-                and flight_payloads[2].is_ssl_client_change_cipher_spec()
-                and flight_payloads[3].is_ssl_server_change_cipher_spec()):
-            ssl_status.ssl_handshake_seen = True
-        else:
-            ssl_status.ssl_handshake_seen = False 
-
-
-    def _ssl_tunnel_analysis(self):
-        pass
-
-
-    def _tcp_close_analysis(self):
-        pass
-
-
-    def _packet_has_payload(self, pkt):
-        return len(pkt[TCP].payload) > 0
-
-    def _tcp_handshake_is_seen(self):
-        '''prereqs: deduplicated, ordered session pair.
-        returns True if we see SYN->SYNACK->ACK sequence in first 3 packets
-        '''
-        session_pair = self
-        cli_to_svr = self._cli_to_svr
-        svr_to_cli = self._svr_to_cli 
-
-        try:
-            if cli_to_svr[0][TCP].flags == 'S' \
-                    and svr_to_cli[0][TCP].flags == 'SA' \
-                    and cli_to_svr[1][TCP].flags == 'A':
-                return True
-
-        except TypeError as e:
-            print "_tcp_handshake_is_seen:", e
-            return False
-
-        return False
