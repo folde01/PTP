e437bdf919bfda9953da96153d39cc125cb4143b
JO <jo6543@yahoo.com>
2018-09-19 19:13:54 +0100

    tidy after submission

--- src/ptp_start.sh
+++ src/ptp_start.sh
@@ -1,45 +1,18 @@
 #!/usr/bin/env bash
 
-if [ $# != 1 ] || ([ $1 != 'virtual' ] && [ $1 != 'physical' ] && [ $1 != 'last' ]); then
-    echo "Usage: $0 <virtual|physical|last>"
-    exit 1
-fi
-
-TARGET_DEVICE_TYPE="$1"
-
-function configure_network() 
-{ 
-    echo "Configuring network."
-    ./ptp_networking.sh $TARGET_DEVICE_TYPE
-    [ $? = 0 ] || ( 
-        echo "Networking setup failed. PTP not starting."
-        exit 1
-    )
-
-    sleep 5
-}
-
 function get_new_cipher_list()
 { 
     echo "Configuring cipher list."
-    ./ptp_cipher_list.sh
+    ./ptp_cipher_list.sh > /dev/null 2>&1
     [ $? = 0 ] || ( 
         echo "Warning: Could not download new cipher list."
     )
 }
 
-function reconfigure_network()
-{ 
-    if [ $TARGET_DEVICE_TYPE != 'last' ]; then 
-        configure_network
-        get_new_cipher_list
-    fi
-}
-
 
 # main
 
-#reconfigure_network
+get_new_cipher_list
 python ptp_init.py reinit
 sudo rm -f sniffed.pcap
 sudo PATH=$PATH python ptp_controller.py
